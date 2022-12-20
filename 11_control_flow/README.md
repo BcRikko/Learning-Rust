6ecc9b5aad6b34eb22b14e37ad80ff933d65e4ed
BcRikko <s.shin1106@gmail.com>
2022-12-20 16:48:47 +0900

    :sparkles: 繰り返し

--- 11_control_flow/README.md
+++ 11_control_flow/README.md
@@ -14,3 +14,33 @@ let num = if condition { 5 } else { 6 };
 - if の条件式は bool でないとエラーになる
   - JSのようなtruthy/falsyのようには使えない
 - ワンライナーでも書ける
+
+
+## ループ
+
+- loop
+- while
+- for
+
+```rs
+// ラベル付きループ
+'label: loop {
+  ...
+  // 'label のループを break(終了)する
+  break 'label;
+}
+
+while condition {
+  ...
+}
+
+let collection = ...
+for item in collection {
+  ...
+}
+
+// Range
+for num in (1..4).rev() {
+  ...
+}
+```
--- 11_control_flow/src/main.rs
+++ 11_control_flow/src/main.rs
@@ -1,4 +1,13 @@
 fn main() {
+    flow_if();
+    flow_loop();
+}
+
+/****************************************
+ * 条件分岐
+ ****************************************/
+
+fn flow_if() {
     let number = 3;
 
     if number < 5 {
@@ -28,4 +37,66 @@ fn fizzbuzz(x: i32) {
     } else {
         println!("{}", x);
     }
+}
+
+/****************************************
+ * 繰り返し
+ ****************************************/
+
+fn flow_loop() {
+    // loop {
+    //     println!("again!");
+    // }
+
+    countup_loop();
+    countdown_while();
+    collection_for()
+}
+
+fn countup_loop() {
+    let mut count = 0;
+
+    'counting_up: loop {
+        println!("count = {}", count);
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {}", remaining);
+            if remaining == 9 {
+                break;
+            }
+
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+
+    println!("End count = {}", count);
+}
+
+fn countdown_while() {
+    let mut number= 3;
+    while number != 0 {
+        println!("{}!", number);
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+}
+
+fn collection_for() {
+    let a = [10, 20, 30, 40, 50];
+    
+    for element in a {
+        println!("The value is: {}", element);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}!", number)
+    }
+    println!("LIFTOFF!!!");
 }
