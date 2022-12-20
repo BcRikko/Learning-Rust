23aad26a4d0c051ddba6f054cbe21fd8c25ae5c3
BcRikko <s.shin1106@gmail.com>
2022-12-20 10:19:17 +0900

    :sparkles: データ型

--- 9_basic/src/main.rs
+++ 9_basic/src/main.rs
@@ -28,4 +28,44 @@ fn main() {
 
     println!("The value of x is: {}", x);
     // > 6
+
+
+    // 四則演算
+    let sum = 5 + 10;
+    println!("Sum: {}", sum);
+
+    let diff = 95.5 - 4.3;
+    println!("Diff: {}", diff);
+
+    let prod = 4 * 30;
+    println!("Prod: {}", prod);
+
+    let quot = 56.7 / 32.2;
+    println!("Quot: {}", quot);
+
+    let floor = 2 / 3;  // i32なので `0` になる
+    println!("Floor: {}", floor);
+    
+    let floor2 = 2.0 / 3.0; // f64なので `0.666...` になる
+    println!("Floor2: {}", floor2);
+    
+    let remainder = 43 % 5;
+    println!("Remainder: {}", remainder);
+
+    let t = true;
+    let f: bool = false;
+
+    let c = 'z';
+    let z: char = 'ℤ';
+    let emoji = '🤔';
+    // 👍🏻 のように色指定が入っていると「character literal may only contain one codepoint」になる
+    println!("Char: {}, {}, {}", c, z, emoji);
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("Tuple1: {0} {1} {2}", x, y, z);
+    println!("Tuple2: {0} {1} {2}", tup.0, tup.1, tup.2);
+
+    let arr: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("Array: {}", arr[0]);
 }
